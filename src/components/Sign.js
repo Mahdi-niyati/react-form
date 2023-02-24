d23607caeace93d65e9d13e490588a8fa91b18f4
Mahdi NIyati <Niyat2612@gmail.com>
2023-02-24 19:26:15 +0330

    Completing the JavaScript logic of the project

--- src/components/Sign.js
+++ src/components/Sign.js
@@ -1,6 +1,10 @@
 import React , {useState , useEffect} from 'react';
 
 import validate from './validate';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { notify } from './toast';
 
 const Sign = () => {
 
@@ -12,11 +16,11 @@ const Sign = () => {
         isAccept:false
     })
     const [errors , setErrors] = useState({});
+    const [focuse , setFocuse] = useState({})
 
 
     useEffect(() => {
         setErrors(validate(data));
-        console.log(errors);
     },[data])
 
     const onchangeHandler = event => {
@@ -28,29 +32,56 @@ const Sign = () => {
 
     }
 
+    const focusHandler = event => {
+            setFocuse({...focuse , [event.target.name]:true})
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+        if(!Object.keys(errors).length){
+            notify("Successfully registered" , "success")
+        }else{
+            notify("Invalid data!" , "error")
+            setFocuse({
+                name:true , 
+                email:true ,
+                password: true ,
+                confirmPassword:true ,
+                isAccept:true
+            })
+        }
+    }
+
     return (
         <div>
-             <form>
+             <form onSubmit={submitHandler}>
                     <div>
                         <label>Name</label>
-                        <input type="text" name='name' value={data.name} onChange={onchangeHandler}  />
+                        <input type="text" name='name' value={data.name} onChange={onchangeHandler} onFocus={focusHandler}  />
+                        {errors.name && focuse.name && <span>{errors.name}</span>}
                     </div>
                     <div>
                         <label>Email</label>
-                        <input type="text" name='email' value={data.email} onChange={onchangeHandler}  />
+                        <input type="text" name='email' value={data.email} onChange={onchangeHandler} onFocus={focusHandler}  />
+                        {errors.email && focuse.email && <span>{errors.email}</span>}
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" name='password' value={data.password} onChange={onchangeHandler}  />
+                        <input type="password" name='password' value={data.password} onChange={onchangeHandler} onFocus={focusHandler}  />
+                        {errors.password && focuse.password && <span>{errors.password}</span>}
                     </div>
                     <div>
                         <label>ConfirmPassword</label>
-                        <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={onchangeHandler}  />
+                        <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={onchangeHandler} onFocus={focusHandler}  />
+                        {errors.confirmPassword && focuse.confirmPassword && <span>{errors.confirmPassword}</span>}
                     </div>
                     <div>
                         <label>Do you accept our rules?</label>
-                        <input type="checkbox" name='isAccept' value={data.isAccept} onChange={onchangeHandler}  />
+                        <input type="checkbox" name='isAccept' value={data.isAccept} onChange={onchangeHandler} onFocus={focusHandler}  />
+                        {errors.isAccept && focuse.isAccept && <span>{errors.isAccept}</span>}
                     </div>
+                    <button type='submit'>Sign Up</button>
+                    <ToastContainer />
              </form>
         </div>
     );
