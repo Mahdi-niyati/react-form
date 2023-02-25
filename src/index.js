e04bf93227fd3d8c5c2ce42d005df035ee8ced58
Mahdi NIyati <Niyat2612@gmail.com>
2023-02-25 11:28:57 +0330

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Figtree:wght@600&family=Lexend+Deca&display=swap');
+</style>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
